65f72e983482606c53f2d59cb3ca5e2733e3b96c
Jeroen Brauns <brauns.jeroen@pm.me>
2021-02-14 11:42:03 -0800

    [improved] syslog script

--- syslog/syslog.py
+++ syslog/syslog.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-# Python script voor het weergeven van de syslog met optie voor aantal regels
+# Python script to show syslog with option for line count
 
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser(description='Python script voor het weergeven van de syslog')
+parser = argparse.ArgumentParser(description='Python script to show syslog with row count')
 
 def get_syslog(rows):
     print(f"=========== SYSLOG ({rows} ROWS) ============")
@@ -16,8 +16,10 @@ def clear_screen():
     subprocess.check_call(["clear"])
 
 
+clear_screen()
+
 # Optional arguments
-parser.add_argument("-r", "--rows", help="Aantal rijen van de syslog", metavar="", type=int)
+parser.add_argument("-r", "--rows", help="Row count for syslog", metavar="", type=int)
 args = parser.parse_args()
 if args.rows:
     clear_screen()
